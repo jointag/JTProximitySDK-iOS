c09eb4fbc256ea2e2e49af71a1e85386f236761b
Marco Fraccaroli <marco.fraccaroli@mwd.digital>
2017-11-17 15:42:44 +0100

    Updated JTProximitySDK to version 1.3.3

--- Library/JTProximitySDK.framework/Headers/JTProximitySDK.h
+++ Library/JTProximitySDK.framework/Headers/JTProximitySDK.h
@@ -5,14 +5,14 @@
 //  Created by Michele Ambrosi on 06/07/17.
 //  Copyright © 2017 Jointag Srl. All rights reserved.
 //
-
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
 #if __IPHONE_OS_VERSION_MAX_ALLOWED >= 100000
-    #import <UserNotifications/UserNotifications.h>
-#endif
 
+#import <UserNotifications/UserNotifications.h>
+
+#endif
 typedef NS_ENUM(NSInteger, JTPLogLevel) {
     JTPLogLevelVerbose,
     JTPLogLevelDebug,
@@ -20,7 +20,6 @@ typedef NS_ENUM(NSInteger, JTPLogLevel) {
     JTPLogLevelWarn,
     JTPLogLevelError
 };
-
 extern NSString *const SDK_VERSION;
 extern NSInteger const SDK_BUILD;
 
@@ -35,25 +34,19 @@ NS_SWIFT_NAME(ProximitySDK)
 
 @property (strong, nonatomic) NSString *apiKey;
 @property (strong, nonatomic) NSString *apiSecret;
-@property (nonatomic, assign, readonly) BOOL debug;
-
+@property (assign, nonatomic, readonly) BOOL debug;
 @property (assign, nonatomic) BOOL promptForPushNotifications;
 @property (assign, nonatomic) BOOL promptForLocationAuthorization;
-
 @property (weak, nonatomic) id<JTProximityCustomDelegate> customDelegate;
-
 + (instancetype)sharedInstance NS_SWIFT_NAME(instance());
 - (void)setLogLevel:(JTPLogLevel)logLevel;
 - (void)initWithLaunchOptions:(NSDictionary *)launchOptions apiKey:(NSString *)apiKey apiSecret:(NSString *)apiSecret;
 - (void)initWithLaunchOptions:(NSDictionary *)launchOptions apiKey:(NSString *)apiKey apiSecret:(NSString *)apiSecret debug:(BOOL)debug;
-
 // Pre iOS 10
 - (BOOL)application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification;
-
 // iOS 10 only
 - (BOOL)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification __IOS_AVAILABLE(10.0);
 - (BOOL)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response __IOS_AVAILABLE(10.0);
-
 - (NSString *)installationId;
 
 @end
